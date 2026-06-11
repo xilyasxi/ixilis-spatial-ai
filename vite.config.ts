06d518d39b88dbae8737dcae073d19b7ed1bf42a
Xilyasxi <xilyas.x@proton.me>
2026-06-11 13:12:42 +0900

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './', // Generates relative asset paths for subpath hosting compatibility (such as GitHub Pages)
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    strictPort: true,
+  },
   build: {
     rollupOptions: {
       input: {
@@ -11,4 +16,5 @@ export default defineConfig({
     },
   },
 });
+
 
